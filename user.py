e46e9d9a8aff1cf945046e2c4ccd56a304602cf8
cuzzo126 <shmoylov2004@mail.ru>
2025-11-18 15:05:07 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,9 @@
+# Запрос данных у пользователя
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Where are you live? ")
+
+# Вывод результатов
+print(f"This is {name}")
+print(f"It is {age}")
+print(f"(S)he live in {city}")
